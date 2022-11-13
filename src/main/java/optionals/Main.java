9baef604e333f68af76e6699d2147b5ae5a1cbe6
Rafael da Silva Santos <05202198155@faqi.edu.br>
2022-11-13 06:10:49 -0300

    Introduction to Optionals. With JDK 17 Doc links.

--- src/main/java/optionals/Main.java
+++ src/main/java/optionals/Main.java
@@ -1,2 +1,29 @@
-package optionals;public class Main {
+package optionals;
+
+import java.util.Optional;
+import java.util.function.Supplier;
+
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Object value = Optional.ofNullable(null)
+                .orElseGet(() -> "Default value");
+
+        Object valueIfNotNullSupplier = Optional.ofNullable("Some value here")
+                .orElseGet(() -> "Default value");
+        
+        Optional.ofNullable("rafael@email.com")
+                .ifPresentOrElse(
+                        email -> out.println("Sending e-mail to "  + email),
+                        () -> out.println("Cannot send e-mail"));
+
+//        Supplier<IllegalStateException> exeption = () -> new IllegalStateException("Exeption");
+//        Object valueExeption = Optional.ofNullable(null)
+//                .orElseThrow(exeption);
+
+
+        out.println(value);
+        out.println(valueIfNotNullSupplier);
+    }
 }
